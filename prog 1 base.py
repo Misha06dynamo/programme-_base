36d1ea93cb4effc1817b123c8663e5d03856d895
Misha06dynamo <mihailaksenov895@yandex.ru>
2024-12-24 20:43:34 +0300

    Add files via upload

---
+++ prog 1 base.py
@@ -0,0 +1,17 @@
+#1st programm
+print(9**0.5*5)
+#2nd programm
+if 9.99>9.98 and 1000!=1000.1:
+    print(True)
+else:
+    print(False)
+#3rd programm
+a=2*2+2
+b=2*(2+2)
+if a==b:
+    print(True)
+else:
+    print(False)
+#4th programm
+a=int((float("123.456")*10)%10)
+print(a)
