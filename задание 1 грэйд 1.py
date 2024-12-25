4d19510e0968808f8ddfb2412dccafe1682b845b
Misha06dynamo <mihailaksenov895@yandex.ru>
2024-12-25 15:39:40 +0300

    Add files via upload

---
+++ задание 1 грэйд 1.py
@@ -0,0 +1,17 @@
+user_name='Michael'
+print("Имя пользователя:",user_name)
+#2
+title=('Zadanye')
+print("Заголовок заметки:",title)
+#3
+content='Rabota nad zametkamy'
+print("Описание заметки:",content)
+#4
+status='+'
+print("Статус заметки:",status)
+#5
+created_date='25-12-2024'
+print("Дата создания:",created_date)
+#6
+issue_date='31-12-2024'
+print("Дата истечения",issue_date)
